61cebe952b919f57c563ada50f9d0378d90e95cf
Joe P <sightper44@gmail.com>
2023-02-05 16:27:11 -0600

    updated colorwool

--- botania/colorWool.lua
+++ botania/colorWool.lua
@@ -1,7 +1,7 @@
 local place_ = "left"
 local break_ = "right"
-local color_1 = "back"
-local color_2 = "front"
+local color_1 = "bottom"
+
 
 function rsOn(side)
     redstone.setOutput(side, true)
@@ -22,18 +22,13 @@ while true do
 
     rsOn(place_)
     sleep(.05)
---    if toggle then
-        rsOn(color_1)
---    else
---        rsOn(color_2)
---    end
+    rsOn(color_1)
     sleep(.1)
     rsOn(break_)
     sleep(.05)
     rsOff(place_)
     rsOff(break_)
     rsOff(color_1)
-    rsOff(color_2)
     sleep(.05)
     toggle = not toggle
 end
