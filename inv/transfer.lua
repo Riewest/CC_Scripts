3d715d52ad2efbd7fd65ae889aa88d4e00825eb6
Joe P <sightper44@gmail.com>
2023-03-02 22:11:02 -0600

    transfer updates

--- inv/transfer.lua
+++ inv/transfer.lua
@@ -1,7 +1,9 @@
 local args = {...}
 local from_inv_name = args[1] or "left"
 local to_inv_name = args[2] or "right"
+local sleep_cycle = tonumber(args[3]) or 0
 local START_DELAY = 4
+local sleep_display = tostring(sleep_cycle).." sec"
 
 function help()
     print("USAGE: transfer [fromSide] [toSide]")
@@ -18,7 +20,8 @@ function invError(message)
     print("Error!")
     shell.run("peripherals")
     print("")
-    error(message)
+    print(message)
+    --error(message)
 end
 
 function preCheck()
@@ -35,10 +38,12 @@ function process_slot(to_inv, from_inv, from_slot)
 end
 
 function process_invs()
-    local from_inv = peripheral.wrap(from_inv_name)
-    local to_inv = peripheral.wrap(to_inv_name)
-    for from_slot,v in pairs(from_inv.list()) do
-        pcall(process_slot, to_inv, from_inv, from_slot)
+    if peripheral.isPresent(to_inv_name) and peripheral.isPresent(from_inv_name) then
+        local from_inv = peripheral.wrap(from_inv_name)
+        local to_inv = peripheral.wrap(to_inv_name)
+        for from_slot,v in pairs(from_inv.list()) do
+            pcall(process_slot, to_inv, from_inv, from_slot)
+        end
     end
 end
 
@@ -46,15 +51,27 @@ function main()
     preCheck()
     print("Startup Delay", START_DELAY)
     sleep(START_DELAY)
-    print("Continuing")
+    term.clear()
+    term.setCursorPos(1,1)
+    print("Transfering items:")
+    print(from_inv_name,"-->",to_inv_name)
+    print("Time:",sleep_display)
     while true do
         pcall(process_invs)
+        sleep(sleep_cycle)
     end
+end
+
+function errorHandler(err)
+    print("Error:", err)
+    print("Rebooting in 5 seconds")
+    sleep(5)
+    os.reboot()
 end
 
 if string.find(string.lower(from_inv_name), "help") then
     help()
 else
-    main()
+    xpcall(main, errorHandler)
 end
 
