d61aa1dd8811dac4e569ea52e0d9a9f3e1f967d0
logicengineer <sightper44@gmail.com>
2025-08-01 14:13:09 -0500

    inspect to text file

---
+++ squid/turtle/inspect.lua
@@ -0,0 +1,18 @@
+-- Attempt to inspect the block in front
+local success, data = turtle.inspect()
+
+-- Open a file called test.lua for writing
+local file = fs.open("info.lua", "w")
+
+-- Write the result to the file
+if success then
+    file.write("Inspected block:\n")
+    file.write(textutils.serialize(data))
+else
+    file.write("No block detected.")
+end
+
+file.close()
+
+print("Inspection written to info.lua")
+
